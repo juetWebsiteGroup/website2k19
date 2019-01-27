eabd4b282c049aa76f047855b26c1bf417644374
Sushant Singh <sushantr134@gmail.com>
2019-01-27 18:13:16 +0530

    JUET PWA FEES Structure Added

---
+++ views/juet-manifest.3bf660e686dbf159abcb6e9a219aceae.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d4831be6dcdcea6c4ac",
+    "url": "server.js"
+  },
+  {
+    "revision": "18aa0896d68647b36e541b1a6b580154",
+    "url": "/app-shell"
+  }
+];
