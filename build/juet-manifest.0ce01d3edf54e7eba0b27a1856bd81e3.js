244ff95723a41d53784db867241ff1829b281766
Sushant Singh <sushantr134@gmail.com>
2019-02-07 23:30:48 +0530

    JUET PWA Code Splitting Added with Admission Updated

---
+++ build/juet-manifest.0ce01d3edf54e7eba0b27a1856bd81e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "341f27bfbf579e5ce86c",
+    "url": "vendor.bundle.js"
+  },
+  {
+    "revision": "b2e71942242d90033e1c",
+    "url": "main.css"
+  },
+  {
+    "revision": "b2e71942242d90033e1c",
+    "url": "juet_bundle.js"
+  },
+  {
+    "revision": "645b6c70f619a119caec",
+    "url": "3.css"
+  },
+  {
+    "revision": "645b6c70f619a119caec",
+    "url": "3.bundle.js"
+  },
+  {
+    "revision": "a6641767717179ffae53",
+    "url": "2.css"
+  },
+  {
+    "revision": "a6641767717179ffae53",
+    "url": "2.bundle.js"
+  },
+  {
+    "revision": "8f2e158693fe132b7ba66eb124065dd3",
+    "url": "/"
+  }
+];
