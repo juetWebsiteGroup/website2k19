9b496654ba1051edf4571b76324e5e1a998c3302
Sushant Singh <sushantr134@gmail.com>
2019-04-11 03:13:27 +0530

    Fb Page and G-map Location added

--- src/juet/LandingPage/UniversityInfo.js
+++ src/juet/LandingPage/UniversityInfo.js
@@ -1,40 +1,36 @@
-import React from 'react'
-import './styles/infoSection.css';
+import React from "react";
+import "./styles/infoSection.css";
 
-const InfoSection = (props) => {
-    return (
-        <div className="Academic">
-        <h1>Academic News</h1>
-        <div className="line"></div>
-        <ul>
-            <li>Anti-Ragging Undertaking, Medical Fitness Certificate Proforma</li>
-            <li>Anti-Ragging Undertaking, Medical Fitness Certificate Proforma</li>
-            <li>Anti-Ragging Undertaking, Medical Fitness Certificate Proforma</li>
-        </ul>
-      </div>
-    )
-}
+const InfoSection = props => {
+	return (
+		<div className='Academic'>
+			<h3>{props.title}</h3>
+			<div className='line' />
+			<ul>
+				<li>Anti-Ragging Undertaking, Medical Fitness Certificate Proforma</li>
+				<li>Anti-Ragging Undertaking, Medical Fitness Certificate Proforma</li>
+				<li>Anti-Ragging Undertaking, Medical Fitness Certificate Proforma</li>
+			</ul>
+		</div>
+	);
+};
 
-export default class UniversityInfo extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
+export default class UniversityInfo extends React.Component {
+	constructor(props) {
+		super(props);
 
-        this.state = {}
-
-    }
-    render()
-    {
-        return(
-            <section className="UniversityInfo">
-              <div className="container">
-                {/*academic News Component */}
-                <InfoSection />
-                <InfoSection />
-                <InfoSection />
-              </div>
-            </section>
-        )
-    }
+		this.state = {};
+	}
+	render() {
+		return (
+			<section className='UniversityInfo'>
+				<div className='container'>
+					{/*academic News Component */}
+					<InfoSection title='Academic News' />
+					<InfoSection title='Announcements' />
+					<InfoSection title='Awards &amp; Achievements' />
+				</div>
+			</section>
+		);
+	}
 }
--- src/juet/LandingPage/bodyMain.jsx
+++ src/juet/LandingPage/bodyMain.jsx
@@ -1,25 +1,24 @@
-import React,{ Component } from 'react';
-import IntroSection from './collegeIntroSection';
+import React, { Component } from "react";
+import IntroSection from "./collegeIntroSection";
 import AdmissionNotification from "./AdmissionNotification";
 // import AnnouncementsCards from './AnnouncementsCards';
-import UniversityInfo from './UniversityInfo';
-
-class BodyMainLandingPage extends Component
-{
-    componentDidMount()
-    {
-        document.title="Jaypee University of Engineering And Technology, Guna"
-    }
-    render(){
-        return(
-            <>
-            <IntroSection/>
-            <AdmissionNotification statusToShow={true}/>
-            {/* <AnnouncementsCards statusToShow={true} /> */}
-            <UniversityInfo announcementsData={null} awardsAchievementData={null} academicNewsData={null} />
-            </>
-        )
-    }
+import UniversityInfo from "./UniversityInfo";
+import VisitUniversity from "./VisitUniversity";
+class BodyMainLandingPage extends Component {
+	componentDidMount() {
+		document.title = "Jaypee University of Engineering And Technology, Guna";
+	}
+	render() {
+		return (
+			<>
+				<IntroSection />
+				<AdmissionNotification statusToShow={true} />
+				{/* <AnnouncementsCards statusToShow={true} /> */}
+				<UniversityInfo announcementsData={null} awardsAchievementData={null} academicNewsData={null} />
+				<VisitUniversity />
+			</>
+		);
+	}
 }
 
 export default BodyMainLandingPage;
