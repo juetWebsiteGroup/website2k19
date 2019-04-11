d49fc9e35410dc1b46fce61cad1181cb905144a6
Sushant Singh <sushantr134@gmail.com>
2019-04-12 04:02:37 +0530

    Student Clubs Added

---
+++ src/juet/LandingPage/StudentClubs.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./styles/studentClub.css";
+
+const ClubCardView = props => {
+  return (
+    <div className='club_Card' onClick={props.showClubView}>
+      <div className='club_container'>
+        <div
+          className='club_photo'
+          style={{
+            background: `url(${props.clubPhotoURL}) no-repeat`,
+            backgroundSize: "cover",
+            backgroundOrigin: "padding-box",
+            backgroundPosition: "center"
+          }}
+        />
+      </div>
+      <div className='club_name'>{props.clubName}</div>
+    </div>
+  );
+};
+
+const ClubMainView = props => {
+  return (
+    <div className='clubMainView'>
+      <img src={props.displayClub.clubPic} alt={props.displayClub.clubName} />
+    </div>
+  );
+};
+
+export default class StudentClubs extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { clubData: [] };
+    this.showClubView = this.showClubView.bind(this);
+  }
+  componentWillMount() {
+    this.setState({
+      clubData: [
+        { id: 1, clubName: "jyc", clubInfo: "jdjdjkadaodio", clubPic: "/static/images/jyc1.jpg", clubPageLink: "https://jyc.org" },
+        {
+          id: 2,
+          clubName: "mozilla",
+          clubInfo: "jfasfsafaodio",
+          clubPic: "/static/images/mozClub1.jpg",
+          clubPageLink: "https://mpcjuet.tech"
+        },
+        { id: 2, clubName: "csi", clubInfo: "ewqqweaodio", clubPic: "/static/images/csi1.jpg", clubPageLink: "https://csi.org" }
+      ]
+    });
+  }
+  showClubView = whichClubObj => {
+    this.setState({ clubView: whichClubObj });
+  };
+  render() {
+    return (
+      <section className='StudentClubs'>
+        <div className='StudentClubs__mainView'>
+          {this.state.clubView != undefined ? (
+            <ClubMainView displayClub={this.state.clubView} />
+          ) : (
+            <ClubMainView displayClub={this.state.clubData[1]} />
+          )}
+        </div>
+        <div className='StudentClubs__container'>
+          <header>
+            <h1>Student Activities Club</h1>
+            <div className='line' />
+          </header>
+          {this.state.clubData.map(clubDataObj => {
+            return (
+              <ClubCardView
+                clubName={clubDataObj.clubName}
+                clubPhotoURL={clubDataObj.clubPic}
+                showClubView={() => this.showClubView(clubDataObj)}
+              />
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
--- src/juet/LandingPage/bodyMain.jsx
+++ src/juet/LandingPage/bodyMain.jsx
@@ -3,22 +3,24 @@ import IntroSection from "./collegeIntroSection";
 import AdmissionNotification from "./AdmissionNotification";
 // import AnnouncementsCards from './AnnouncementsCards';
 import UniversityInfo from "./UniversityInfo";
+import StudentClubs from "./StudentClubs";
 import VisitUniversity from "./VisitUniversity";
 class BodyMainLandingPage extends Component {
-	componentDidMount() {
-		document.title = "Jaypee University of Engineering And Technology, Guna";
-	}
-	render() {
-		return (
-			<>
-				<IntroSection />
-				<AdmissionNotification statusToShow={true} />
-				{/* <AnnouncementsCards statusToShow={true} /> */}
-				<UniversityInfo announcementsData={null} awardsAchievementData={null} academicNewsData={null} />
-				<VisitUniversity />
-			</>
-		);
-	}
+  componentDidMount() {
+    document.title = "Jaypee University of Engineering And Technology, Guna";
+  }
+  render() {
+    return (
+      <>
+        <IntroSection />
+        <AdmissionNotification statusToShow={true} />
+        {/* <AnnouncementsCards statusToShow={true} /> */}
+        <UniversityInfo announcementsData={null} awardsAchievementData={null} academicNewsData={null} />
+        <StudentClubs />
+        <VisitUniversity />
+      </>
+    );
+  }
 }
 
 export default BodyMainLandingPage;
