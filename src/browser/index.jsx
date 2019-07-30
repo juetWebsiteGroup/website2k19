791e51c75c9ba54d0a6e4168b01bfce1079ba530
Sushant Singh <sushantr134@gmail.com>
2019-07-30 23:51:53 +0530

    Alliance Added

--- src/browser/index.jsx
+++ src/browser/index.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {BrowserRouter as Router} from 'react-router-dom'
-import IndexRender from '../main.jsx'
-import * as webfont from 'webfontloader'
-import window from 'global'
-import {Provider} from 'react-redux'
-import configureStore from '../redux/configureStore'
-import {detect} from 'detect-browser'
-import Loadable from 'react-loadable'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import IndexRender from "../main.jsx";
+import * as webfont from "webfontloader";
+import window from "global";
+import { Provider } from "react-redux";
+import configureStore from "../redux/configureStore";
+import { detect } from "detect-browser";
+import Loadable from "react-loadable";
 
 webfont.load({
-    google:{
-        families:['Lato','Dosis','Courgette','Alegreya Sans','Abel','Playfair Display','sans-serif','cursive']
-    },
-    timeout:2000
-    })
-    
+  google: {
+    families: ["Lato", "Dosis", "Courgette", "Alegreya Sans", "Abel", "Playfair Display", "sans-serif", "cursive"]
+  },
+  timeout: 2000
+});
+
 const state = window.__DATA_FROM_SERVER__;
 delete window.__DATA_FROM_SERVER__;
 
-const juetStore = configureStore(state)
+const juetStore = configureStore(state);
 const browser = detect();
 
 console.log(browser);
-if(browser.name === 'ie')
-{
-    window.location = "https://www.juet.ac.in"
+if (browser.name === "ie") {
+  window.location = "https://www.juet.ac.in";
 }
 
-Loadable.preloadReady().then(()=>{
-    ReactDOM.hydrate(
-        <Provider store={juetStore}>
-        <Router>
-           <IndexRender />
-       </Router>
-        </Provider>   
-       ,document.getElementById('juetroot'))
-})
+Loadable.preloadReady().then(() => {
+  ReactDOM.hydrate(
+    <Provider store={juetStore}>
+      <Router>
+        <IndexRender />
+      </Router>
+    </Provider>,
+    document.getElementById("juetroot")
+  );
+});
--- src/juet/LandingPage/PlacementsAndAlliances.js
+++ src/juet/LandingPage/PlacementsAndAlliances.js
@@ -1,7 +1,61 @@
 import React from "react";
+import logo from "../../../static/images/UOF.png";
+import logo2 from "../../../static/images/A4U.jpg";
+import logo3 from "../../../static/images/UON.jpg";
+import logo4 from "../../../static/images/cbri-logo.jpg";
+import { InnerPageContainer } from "../../containers/InnerPageContainer/index";
+
+import "./styles/placementalliance.scss";
+
+const Alliance = props => {
+  return (
+    <div className='AlliancesContainer'>
+      <div className='Heading'>
+        <h1>Academic Alliance</h1>
+        <div className='line' />
+      </div>
+      <ul>
+        <li>
+          <div className='AllianceItem'>
+            <img src={logo} />
+            <a href='/'>MOU Between The University of Florida & JUET, Guna.</a>
+          </div>
+        </li>
+        <li>
+          <div className='AllianceItem'>
+            <img src={logo2} />
+            <a href='/'>General Memerandum of Understanding Between the Alliance of A universities (A-4U) of Spain & JUET, Guna.</a>
+          </div>
+        </li>
+        <li>
+          <div className='AllianceItem'>
+            <img src={logo3} />
+            <a href='/'>MOU Between University of Nebraska at OMAHA, USA & JUET, Guna.</a>
+          </div>
+        </li>
+        <li>
+          <div className='AllianceItem'>
+            <img src={logo4} />
+            <a href='/'>
+              MOU Between JUET, Guna and Council of Scientific and Industrial Research (CSIR) through Central Building Research Institute
+              (CSIR-CBRI), Roorkee.
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default class PlacementsAlliance extends React.Component {
   render() {
-    return <h1>Placements</h1>;
+    return (
+      <InnerPageContainer>
+        <>
+          <Alliance />
+        </>
+        <div>Placements</div>
+      </InnerPageContainer>
+    );
   }
 }
