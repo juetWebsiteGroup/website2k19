e53ad2b1580276a8aadf190e4e812b0035eb4e15
Sushant Singh <sushantr134@gmail.com>
2019-07-31 05:05:51 +0530

    Lazy Loading added enhanced footer fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,6 @@ import { AdmissionFooter } from "./juet/Admissions/tempAdm/AdmissionFooter";
 import { routes } from "./Routes.jsx";
 import "./juet/LandingPage/styles/body.css";
 
-import LazyLoad from "react-lazyload";
-
 import { ErrorPageNotFound } from "./juet/NotFound/NotFound.jsx";
 class IndexRender extends React.PureComponent {
   constructor(props) {
@@ -30,9 +28,8 @@ class IndexRender extends React.PureComponent {
           <Route component={ErrorPageNotFound} />
         </Switch>
         {/* <AdmissionFooter /> */}
-        <LazyLoad once>
-          <FooterMake />
-        </LazyLoad>
+
+        <FooterMake />
       </div>
     );
   }
